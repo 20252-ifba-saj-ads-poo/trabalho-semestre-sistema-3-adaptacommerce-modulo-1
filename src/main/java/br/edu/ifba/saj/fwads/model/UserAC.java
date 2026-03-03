0719a8e5f262177d27109947f2067ec82fe811c1
clads02 <20242tadssaj0002@ifba.edu.br>
2026-03-03 02:58:42 -0300

    UserAC

--- src/main/java/br/edu/ifba/saj/fwads/model/UserAC.java
+++ src/main/java/br/edu/ifba/saj/fwads/model/UserAC.java
@@ -1,13 +1,21 @@
 package br.edu.ifba.saj.fwads.model;
 
 public class UserAC {
-    int id;
+   private int id;
     String name;
-    int senha;
-    public User(int id, String name, int senha) {
+    public String senha;
+    public UserAC(int id, String name, String senha) {
         this.id = id;
         this.name = name;
         this.senha = senha;
-    }
+    }  
+    public void ValidSenha(String senha) { if (
+        senha.length() <= 6 && senha.length() >= 1 ) { this.senha = senha; System.out.println("Senha válida");}
+        else System.err.println("Senha inválida");}
+    public void ValidNome(String nome) { if (
+        nome.length() > 2 && nome.length() < 100 ) {System.err.println("Nome válido");}
+        else System.err.println("Nome inválido");}
     
-}
+    public int getId() {
+        return id;}
+    }
