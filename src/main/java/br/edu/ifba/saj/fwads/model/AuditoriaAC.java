8b89a36a0ae9bdd05b512a7d1d67688f7a6fe960
clads02 <20242tadssaj0002@ifba.edu.br>
2026-03-03 23:58:39 -0300

    edit1

--- src/main/java/br/edu/ifba/saj/fwads/model/AuditoriaAC.java
+++ src/main/java/br/edu/ifba/saj/fwads/model/AuditoriaAC.java
@@ -9,14 +9,23 @@ public class AuditoriaAC {
   public List<Adm> Adm = new ArrayList<>();
 
   private LocalDateTime dataHora;
-  private String acao;
-  private UserAC usuario;
+    private String acao;
+    private UserAC usuario;
 
-    public AuditoriaAC(LocalDateTime dataHora, String acao, UserAC usuario) {
+  public AuditoriaAC(LocalDateTime dataHora, String acao, UserAC usuario) {
         this.dataHora = dataHora;
         this.acao = acao;
         this.usuario = usuario;
-
       
+
+    }
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+    public String getAcao() {
+        return acao;
+        }
+    public UserAC getUsuario() {
+        return usuario;
     }
 };
